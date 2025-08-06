baaf42ac7b7463ee4e04c8138afed96ffaaa0717
Twenty Qwerty <vanyka.anishenko@gmail.com>
2025-08-06 15:53:44 +0300

    zaebalsya

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: './',
+  base: '/',
   build: {
     outDir: 'dist',
+    sourcemap: true,
   },
   server: {
     host: true,
